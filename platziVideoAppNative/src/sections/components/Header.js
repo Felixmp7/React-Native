32bbf2739b6c174e72a047209bd29240b22432be
Felixmp_a <felixmanuelpachecoalonso@gmail.com>
2019-05-28 11:12:50 -0400

    Importando componentes de react y de native y construyendo componente

---
+++ platziVideoAppNative/src/sections/components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import {
+  View,
+  Text,
+  Image,
+  StyleSheet
+} from 'react-native'
+
+const Header = props => {
+  return (
+    <View>
+      <Text>Éste es el Header</Text>
+    </View>
+  );
+}
+
+export default Header
