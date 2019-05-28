9d61fdf34c5a78054283357d743481c3f0f74473
Felixmp_a <felixmanuelpachecoalonso@gmail.com>
2019-05-28 11:15:03 -0400

    Importando componente de Header y renderizandolo en Home

--- platziVideoAppNative/App.js
+++ platziVideoAppNative/App.js
@@ -1,14 +1,17 @@
+//React and Native Components
 import React, {Component} from 'react';
 import {Text} from 'react-native';
-
+//Containers
 import Home from './src/screens/containers/Home'
+//Components
+import Header from './src/sections/components/Header'
 
 type Props = {};
 export default class App extends Component<Props> {
   render() {
     return (
       <Home>
-        <Text>Aqui va el Header</Text>
+        <Header/>
         <Text>Aqui va el buscador</Text>
         <Text>Aqui van categorias</Text>
         <Text>Aqui van las Sugerencias</Text>
