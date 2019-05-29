44993a0019a6d812624aa9ccdaf8a2e2106eae1c
Felixmp_a <felixmanuelpachecoalonso@gmail.com>
2019-05-29 11:19:43 -0400

    Guardando en un query el fetch

--- platziVideoAppNative/src/utils/API.js
+++ platziVideoAppNative/src/utils/API.js
@@ -1,1 +1,9 @@
 const BASE_API = "https://yts.am/api/v2"
+
+class Api {
+  async getSuggestions(id){
+    const query = await fetch(`${BASE_API}/movie_suggestions.json?movie_id=${id}`)
+  }
+}
+
+export default new Api()
