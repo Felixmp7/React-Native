b54bf8d77e5501ba08b3f0c4233d2d51a539ac22
Felixmp_a <felixmanuelpachecoalonso@gmail.com>
2019-05-28 20:56:06 -0400

    Haciendo que el separator sea dinamico

--- platziVideoAppNative/src/videos/components/VerticalSeparator.js
+++ platziVideoAppNative/src/videos/components/VerticalSeparator.js
@@ -7,10 +7,21 @@ import {
 
 const Separator = props => {
   return (
-    <View>
+    <View style={[
+      styles.separator,
+      {
+        borderTopColor: (props.color) ? props.color : '#ddd',
+      }
+    ]}>
       <Text>Esto es un separador</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  separator: {
+    borderTopWidth: 1,
+  }
+})
 
 export default Separator
