6c554f7d13a6cbf8cd97240724bc201229e55575
Felixmp_a <felixmanuelpachecoalonso@gmail.com>
2019-06-03 20:37:52 -0400

    Ignore

--- platziVideoAppNative/src/utils/API.js
+++ platziVideoAppNative/src/utils/API.js
@@ -3,7 +3,6 @@ const BASE_API = "https://yts.am/api/v2"
 class Api {
   async getSuggestions(id){
     try{
-      console.log('Intento de peticion 1');
       const query = await fetch(`${BASE_API}/movie_suggestions.json?movie_id=${id}`)
       //Para parsear los datos que me llegan de la API utilizo el metodo json()
       const { data } = await query.json()
@@ -17,7 +16,6 @@ class Api {
 
   async getMovies(){
       try{
-        console.log('Intento de peticion 2');
         const query = await fetch(`${BASE_API}/list_movies.json?`)
         const { data } = await query.json()
 
