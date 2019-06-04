8d2f80e0beec42133bfe50bec5f8ee6c6f48a13b
Felixmp_a <felixmanuelpachecoalonso@gmail.com>
2019-06-03 20:35:41 -0400

    Añadiendo estilos a las categorias

--- platziVideoAppNative/src/videos/components/Category.js
+++ platziVideoAppNative/src/videos/components/Category.js
@@ -1,11 +1,43 @@
 import React from 'react'
-import {View, Text} from 'react-native'
+import {
+  View,
+  Text,
+  ImageBackground,
+  StyleSheet
+} from 'react-native'
 
 const Category = props => {
   return (
-    <View>
-      <Text>{props.genres[0]}</Text>
-    </View>
+    <ImageBackground
+      style={styles.wrapper}
+      source={{
+        uri: props.background_image
+      }}
+      >
+      <Text style={styles.genre}>{props.genres[0]}</Text>
+    </ImageBackground>
   )
 }
+
+const styles = StyleSheet.create({
+  wrapper: {
+    width: 250,
+    height: 100,
+    borderRadius: 10,
+    overflow: 'hidden',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  genre: {
+    color: 'white',
+    fontSize: 40,
+    fontWeight: 'bold',
+    textShadowColor: 'rgba(0,0,0, .75)',
+    textShadowOffset: {
+      width: 2,
+      height: 2,
+    },
+    textShadowRadius: 10
+  }
+})
  export default Category
