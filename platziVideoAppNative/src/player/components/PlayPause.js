d019b0c41529914f63bef2626c9b9ec6e6640c72
Felixmp_a <felixmanuelpachecoalonso@gmail.com>
2019-06-05 17:09:41 -0400

    Creando componente de PlayPause y conociendo nuevos componentes Touchables

---
+++ platziVideoAppNative/src/player/components/PlayPause.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import {
+  TouchableHighlight, //Cuando mantengo presionado
+  TouchableOpacity, // Justo al momento de hacer tab
+  TouchableWithoutFeedback // Reacciona sin efecto alguno
+} from 'react-native'
+
+const PlayPause = props => {
+  return (
+
+  );
+}
+
+export default PlayPause
